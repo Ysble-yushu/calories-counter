f337e574fbe3ba39503161dc5477d1de4021dc7a
Ysble-yushu <gys200922@163.com>
2023-10-07 14:25:22 +0200

    created data file

---
+++ module/calories_counter.py
@@ -0,0 +1,30 @@
+meals  = {
+   'Hamburger': 600,
+   'Cheese Burger': 750,
+   'Veggie Burger': 400,
+   'Vegan Burger': 350,
+   'Sweet Potatoes': 230,
+   'Salad': 15,
+   'Iced Tea': 70,
+   'Lemonade': 90,
+}
+
+combos = {
+    "Cheesy Combo": ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+    "Veggie Combo": ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+    "Vegan Combo": ["Vegan Burger", "Salad", "Lemonade"],
+}
+
+def calorie_counter(*items):
+    total = 0
+    for item in items:
+        if item in meals:
+            total += meals[item]
+        else:
+            try:
+                combos_items = combos[item]
+                for combo_item in combos_items:
+                    total += meals[combo_item]
+            except KeyError:
+                print(f"{item} is not in the menu")
+    return total
